b0c6115c4fea5694bd771743f0dfd73773190c61
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-10-18 11:34:58 -0300

    feat: ensure SignUpController retrurns 400 if no password is provided

--- src/presentation/controllers/signup-controller/signup.controller.ts
+++ src/presentation/controllers/signup-controller/signup.controller.ts
@@ -7,7 +7,7 @@ import {
 
 export class SignUpController {
   handle (httpRequest: HttpRequestProtocol): HttpResponseProtocol {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
 
     for (const field of requiredFields) {
       if (!httpRequest?.body[field]) {

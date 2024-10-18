f94fb7481032ab5d00cb19178b300871c56c93a7
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-10-18 11:33:32 -0300

    refactor: simplify code

--- src/presentation/controllers/signup-controller/signup.controller.ts
+++ src/presentation/controllers/signup-controller/signup.controller.ts
@@ -7,12 +7,12 @@ import {
 
 export class SignUpController {
   handle (httpRequest: HttpRequestProtocol): HttpResponseProtocol {
-    if (!httpRequest?.body?.name) {
-      return badRequest(new MissingParamError('name'))
-    }
+    const requiredFields = ['name', 'email']
 
-    if (!httpRequest?.body?.email) {
-      return badRequest(new MissingParamError('email'))
+    for (const field of requiredFields) {
+      if (!httpRequest?.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
     return {

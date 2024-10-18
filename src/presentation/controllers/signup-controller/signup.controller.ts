f3e7e57c5f5677cd0340c11bd0d18813d1917205
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-10-18 12:04:00 -0300

    feat: ensure SignUpController retrurns 400 if an invalid email is provided

--- src/presentation/controllers/signup-controller/signup.controller.ts
+++ src/presentation/controllers/signup-controller/signup.controller.ts
@@ -1,8 +1,15 @@
-import { MissingParamError } from '../../erros'
+import { InvalidParamError, MissingParamError } from '../../erros'
 import { badRequest } from '../../helpers'
-import { type ControllerProtocol, type HttpRequestProtocol, type HttpResponseProtocol } from '../../protocols'
+import {
+  type EmailValidatorProtocol,
+  type ControllerProtocol,
+  type HttpRequestProtocol,
+  type HttpResponseProtocol
+} from '../../protocols'
 
 export class SignUpController implements ControllerProtocol {
+  constructor (private readonly emailValidator: EmailValidatorProtocol) {}
+
   handle (httpRequest: HttpRequestProtocol): HttpResponseProtocol {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -10,6 +17,12 @@ export class SignUpController implements ControllerProtocol {
       if (!httpRequest?.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email as string)
+
+    if (!isEmailValid) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     return {
--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from './http-response.protocol'
 export * from './http-request.protocol'
 export * from './controller.protocol'
+export * from './email-validator.protocol'
